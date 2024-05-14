8febfa71db766f34192673925e33ad9e03d3e7a9
v-r-b <volker@buerckel.de>
2024-05-14 15:17:45 +0200

    refactor: Adapt sample calls to recent changes in __init__.py

--- tests/t_init.py
+++ tests/t_init.py
@@ -2,33 +2,43 @@
 Sample usage of functions from __init__.py
 """
 
-import userinput_vrb
+from userinput_vrb import *
 
 ###########################################################
 # SAMPLE USAGE: input_with_default
 ###########################################################
 
-c = userinput_vrb.input_with_default("Input Color", "red")
+c = input_with_default("Input Color", "red")
 print(f"You have entered '{c}'")
 
 ###########################################################
 # SAMPLE USAGE: getpass_not_empty
 ###########################################################
 
-p = userinput_vrb.getpass_not_empty("Enter hidden password: ")
+p = getpass_not_empty("Enter hidden password: ")
+print(f"You have entered '{p}'")
+
+def pass_check_fn(passwd: str) -> bool:
+    if passwd.find(".") >= 0:
+        return True
+    else:
+        print("Password must contain a '.' character! Please reenter.")
+        return False
+    
+p = getpass_not_empty("Enter hidden password: ", check_fn=pass_check_fn)
 print(f"You have entered '{p}'")
 
 ###########################################################
 # SAMPLE USAGE: choose_from
 ###########################################################
 
-if "y" != userinput_vrb.choose_from("Do you want to continue? ", "yn", ": ", "n"):
+if "y" != choose_from("Do you want to continue? ", "yn", end=": ", dflt="n"):
     print("ABORTED!")
 else:
     print("GO AHEAD!")
 
-print("Choose again, this time w/o default", end=": ")
-c = userinput_vrb.choose_from()
+print("Choose again, this time w/o default", end=". ")
+c = choose_from()
 print(f"You have chosen '{c}'")
 
 
